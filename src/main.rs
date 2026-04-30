f598cc2d86e8bbcd1f960619e9078a1f39635991
atlanssia <hi@atlanssia.com>
2026-04-30 21:36:32 +0800

    fix: set default log level to info when RUST_LOG is unset

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,11 @@ enum RoutesCommand {
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .init();
 
     let cli = Cli::parse();
     let data_dir = cli.data_dir.unwrap_or_else(|| {
